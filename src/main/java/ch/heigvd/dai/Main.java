bca5867f8d1f9aa01f412a137caaca8765c17746
Davtek11 <david.berger@heig-vd.ch>
2024-10-13 18:16:09 +0200

    first commit picocli

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,9 +1,52 @@
 package ch.heigvd.dai;
 
-public class Main {
+import java.io.File;
+import picocli.CommandLine;
+import java.util.concurrent.Callable;
+
+
+
+@CommandLine.Command(
+        name = "txtToASCIIArt",
+        description = "Convert a text from a input.txt file into an ASCII art in the output.txt file",
+        version = "1.0",
+        mixinStandardHelpOptions = true
+)
+
+public class Main implements Callable<Integer> {
+
+    @CommandLine.Option(
+            names = {"-i", "--input"},
+            description = "Fichier texte d'entrée",
+            required = true)
+    private File inputFile;
+
+    @CommandLine.Option(names = {"-o", "--output"},
+            description = "Fichier texte de sortie",
+            required = true)
+    private File outputFile;
+
+    @CommandLine.Option(names = {"-a", "--art"},
+            description = "Choix de l'ASCII Art (1, 2 ou 3)",
+            required = true)
+    private int asciiArtChoice;
+
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
 
-        System.out.println("Hello world!");
+    @Override
+    public Integer call() {
+        System.out.println("Fichier d'entree : " + inputFile.getAbsolutePath());
+        System.out.println("Fichire de sortie : " + outputFile.getAbsolutePath());
+        System.out.println("ASCII Art choisi : " + asciiArtChoice);
 
+        //String textToConvert = read(inputFile);
+        //convertText
+        //Write
+
+        return 0;
     }
 }
